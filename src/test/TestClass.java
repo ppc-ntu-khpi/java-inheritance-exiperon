cbaf3438504987ccdd60f0be9e86c89c4219d281
exiperon <67110285+exiperon@users.noreply.github.com>
2022-02-18 12:18:15 +0200

    Add files via upload

---
+++ src/test/TestClass.java
@@ -0,0 +1,36 @@
+package test;
+
+import domain.Animal;
+import domain.Human;
+
+public class TestClass {
+    public static void main(String[] args) {
+        Human human = new Human("Name", 1);
+        human.getGender();
+        human.setGender(new Animal.Type());
+        human.getAge();
+        human.setAge(0);
+        human.isMammal();
+        human.setMammal(true);
+        human.getSpeciesName();
+        human.setSpeciesName("Human");
+        human.eat();
+        human.communicate();
+        human.breathe();
+        human.sleep();
+
+        human.hairOrFur();
+        human.setHairOrFur(true);
+        human.isPredator();
+        human.setPredator(false);
+        human.hunt();
+
+        human.getName();
+        human.setName("Name");
+        human.getId();
+        human.setId(0);
+        human.learn();
+        human.work();
+        human.rest();
+    }
+}
